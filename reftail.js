ce7430f4e497c9ccee4d2678379279501e77da24
Christopher Käck <christopher.kack@gmail.com>
2012-08-10 14:02:47 +0200

    changed url

---
+++ reftail.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	function getUrlVars() {
+	    var vars = {};
+	    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	        vars[key] = value;
+	    });
+	    return vars;
+	}
+	ref = getUrlVars()["ref"];
+	host = window.location.hostname;
+	console.log(ref);
+	if (ref !== undefined) {
+    	$.ajax({
+		  url: "http://reftail.kejsarmakten.se/add",
+		  data: {"ref": ref, "host": host }
+		});
+	}
+    $('.tictail_add_to_cart_button').bind('click', function() {
+    	$.ajax({
+		  url: "http://reftail.kejsarmakten.se/sell",
+		  data: {"ref": ref, "host": host, "title":document.title}
+		});
+    });
+});
